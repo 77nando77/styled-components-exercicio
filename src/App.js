4fa6975a7fda89b15bd40ab7056d71f114bc341d
luizfk <luiz.kessler7@gmail.com>
2022-10-14 14:30:37 -0300

    styled

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import "./styles.css";
 // import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
 
-// const GlobalStyle = createGlobalStyle``
+// const GlobalStyle = createGlobalStyle`
+// *{
+//     margin: 0;
+//     paddin: 0;
+//     box-sizing: border-box;
+// }
+// `
   
 
 const MudaHeader = styled.header`
